768e28446a8816944c0e85fb4acd06d5de67bd54
Harim_Kim <yongkal2@yonsei.ac.kr>
2017-01-04 16:33:14 +0900

    Log-in, admin, Excel done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
   end
+  def excel
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root 'home#index'
+  get "home/excel"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
