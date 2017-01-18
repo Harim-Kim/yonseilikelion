68434c3702eeab14bf7aa70427101a1bc3865e3b
Harim_Kim <yongkal2@yonsei.ac.kr>
2017-01-18 15:09:49 +0900

    test1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  root 'home#index'
+  get "home/index"
   get "home/excel"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
